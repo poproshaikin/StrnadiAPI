53ea6c2e7d961dd29b9f463846aaad24af7aa8e5
poproshaikin <@poproshaikin>
2025-01-25 22:30:56 +0100

    add class providing methods to a binary sound files saving

--- StrnadiAPI/Data/Repositories/FileSystemHelper.cs
+++ StrnadiAPI/Data/Repositories/FileSystemHelper.cs
@@ -1,9 +1,29 @@
+using StrnadiAPI.Data.Models.Database;
+
 namespace StrnadiAPI.Data.Repositories;
 
 public class FileSystemHelper
 {
-    public void SaveRecordingSoundFile(int recordingId, int recordingPartId, byte[] data)
+    private readonly string _pathToRecordingsDirectory = $"{AppDomain.CurrentDomain.BaseDirectory}/binRecordings/";
+    
+    /// <returns>Path of the generated file</returns>
+    public string SaveRecordingSoundFile(int recordingId, int recordingPartId, byte[] data)
     {
-        
+        CreateDirectoryIfNotExists(recordingId);
+
+        string path = _pathToRecordingsDirectory + $"{recordingId}_{recordingPartId}.wav";
+        File.WriteAllBytes(path, data);
+
+        return path;
+    }
+
+    private void CreateDirectoryIfNotExists(int recordingId)
+    {
+        string path = _pathToRecordingsDirectory + recordingId;
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
     }
 }
