71e530b6ea778ad27c4e9f573fb2ebcace74c65b
poproshaikin <@poproshaikin>
2025-01-28 09:00:54 +0100

    File providing methods for logging

---
+++ StrnadiAPI/Services/Logger.cs
@@ -0,0 +1,9 @@
+namespace StrnadiAPI.Services;
+
+public static class Logger
+{
+    public static void Log(string message)
+    {
+        Console.WriteLine($"{DateTime.Now} -> {message}");
+    }
+}
